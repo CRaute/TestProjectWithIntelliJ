ad9bb3fcea89bbb314127227847d2d2352f06471
CRaute <neueMail67@gmail.com>
2017-07-20 22:47:24 +0200

    Added project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,28 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan
 public class Main {
+
+    @Bean
+    MessageService mockMessageService() {
+        return new MessageService() {
+            public String getMessage() {
+                return "Hello World!";
+            }
+        };
+    }
+
+
     public static void main(String[] args){
         Haut haut = Haut.FELL;
         LandTier hase = new LandTier(12, 20, haut);
         System.out.println(hase.toString());
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(Main.class);
+        MessagePrinter printer = context.getBean(MessagePrinter.class);
+        printer.printMessage();
     }
 }
